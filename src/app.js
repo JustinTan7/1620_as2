786048df9b7c1a6c81f0d1404fa6618dc10f39b1
Justin Tan <Justin>
2022-04-08 13:08:17 -0700

    made plus button to open text area and cancel button to close textarea

--- src/app.js
+++ src/app.js
@@ -5,3 +5,43 @@ const notes = [
     id: 1 
   }
 ]
+function noteTemplate(){
+  const textTemplate = `<textarea name="" id=""cols="50" rows="20"></textarea>
+                        <button class = "save">Save</button>
+                        <button class= "cancel">Cancel</button>`
+  return textTemplate
+}
+
+function blankTemplate(){
+  const blankT = ``
+  return blankT
+}
+
+function newTextArea(note){
+  const newTextDisplay = document.querySelector('.write-note-area')
+  newTextDisplay.innerHTML = ''
+  newTextDisplay.insertAdjacentHTML('beforeend', note)
+}
+
+function noteGet(){
+  const text = document.querySelector('.icons')
+  const note = text.value
+  return note
+}
+
+function deleteText(){
+  const note = blankTemplate()
+  newTextArea(note)
+}
+
+function displayNote(){
+  const text = noteGet()
+  const note = noteTemplate(text)
+  newTextArea(note)
+  const cancelBtn = document.querySelector(".cancel") 
+  cancelBtn.addEventListener('click', deleteText)
+}
+
+plusButton = document.querySelector('.icons')
+plusButton.addEventListener('click', displayNote)
+
