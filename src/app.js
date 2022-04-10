81ee09332127c4847304f00b58080d9f049515c9
Justin Tan <Justin>
2022-04-10 14:43:09 -0700

    added a function to take the text value from the text area and return it

--- src/app.js
+++ src/app.js
@@ -6,17 +6,17 @@ const notes = [
   }
 ]
 
-//defining variables that will be used
+// defining variables that will be used
 const newNoteArea = document.querySelector(".write-note-area")
 const addNoteBtn = document.querySelector(".fa-circle-plus")
 const textArea = `<textarea name="" class="textArea" cols="60" rows="30"></textarea>`
 const saveBtn = `<button class="saveBtn">save</button>`
 const cancelBtn = `<button class="cancelBtn">cancel</button>`
-const sideBar = document.querySelector("notes-list")
+const sideBar = document.querySelector(".notes-list")
 
 addNoteBtn.addEventListener("click", (displayNote))
 
-//function that displays the note
+// function that displays the note
 function displayNote() {
   newNoteArea.insertAdjacentHTML("beforeend", textArea)
   newNoteArea.insertAdjacentHTML("beforeend",saveBtn)
@@ -26,10 +26,19 @@ function displayNote() {
   cancelNoteBtn.addEventListener('click', cancelNote) 
 }
 
-//functionality for + button
+// functionality for + button
 addNoteBtn.addEventListener("click", (displayNote))
 
+// functionality for the cancel button
 function cancelNote() {
   addNoteBtn.addEventListener("click",displayNote)
   newNoteArea.innerHTML = ''
 }
+
+// function that takes the whole note and returns it
+function getNote(){
+  const text = document.getElementById("textArea")
+  const noteText = text.value
+  return noteText
+}
+
