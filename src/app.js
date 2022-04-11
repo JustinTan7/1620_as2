cb2ebdd72319d42dd6ba09a4c05a8990e2ab2f70
Justin Tan <Justin>
2022-04-10 17:23:40 -0700

    added a function that adds notes to array, and another one that allows opening saved notes

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const addNoteBtn = document.querySelector(".fa-circle-plus")
 const template = `<textarea name="" class="textArea" cols="60" rows="30"></textarea>
                   <button class="saveBtn">save</button>
                   <button class="cancelBtn">cancel</button>`
-const sideBar = document.querySelector(".notes-list")
 
 addNoteBtn.addEventListener("click", (displayNote))
 
@@ -33,6 +32,13 @@ addNoteBtn.addEventListener("click", (displayNote))
 function cancelNote() {
   addNoteBtn.addEventListener("click",displayNote)
   newNoteArea.innerHTML = ''
+}
+
+// function that closes a ready only note
+function cancelReadOnly(){
+  addNoteBtn.addEventListener('click', displayNote)
+  const readOnlyArea = document.querySelector('.read-note-area')
+  readOnlyArea.innerHTML = ''
 }
 
 // function that takes the whole note and returns it
@@ -57,6 +63,32 @@ function addTitleToSideNav(){
   li.className = "noteTitle"
   li.appendChild(document.createTextNode(getTitle()))
   selectNav.append(li)
+  const lastNote = selectNav.lastChild
+  lastNote.addEventListener('click', (evt) =>{
+    newTitle = evt.target.innerHTML
+    readNote(newTitle)
+    console.log('hello')
+  })
+}
+
+// function that adds the note into the array
+function addNoteIntoArray(){
+  const fullNote = getNote()
+  const splitNote = fullNote.split("\n")
+  const index = 0
+  var finalNote = ""
+  if (index > -1){
+    splitNote.splice(index, 1);
+  }
+  for (const x of splitNote){
+    finalNote += x;
+    finalNote += "\n";
+  }
+  notes.push({
+    title: getTitle(),
+    noteBody: finalNote,
+    id: notes.length + 1
+  })
 }
 
 // function that adds the notes into notes array of objects
@@ -71,11 +103,34 @@ getInitialNote()
 
 // functionality for the save button
 function saveNote() {
-  getNote()
   getTitle()
+  addNoteIntoArray()
   addTitleToSideNav()
   cancelNote()
   }
+
+  // function that opens a saved noted and adds a button that can close the note
+function readNote(title){
+  const readOnlyTemplate = `<textarea name="" class="textArea" cols="60" rows="30"></textarea>
+                            <button class="closeBtn">Close</button>`
+  const readNoteTitle = document.querySelector(".noteTitle")
+  var finalNote = ''
+  console.log(readNoteTitle.innerHTML)
+  for (x of notes){
+    console.log(x.title)
+    if (title == x.title){
+      finalNote = x.title + '\n' + x.noteBody
+      console.log(finalNote)
+    }
+  const readOnly = document.querySelector('.read-note-area')
+  readOnly.innerHTML = readOnlyTemplate
+  const readTextArea = document.querySelector('.textArea')
+  readTextArea.innerHTML = finalNote
+  const closeButton = document.querySelector('.closeBtn')
+  closeButton.addEventListener('click', cancelReadOnly)
+  addNoteBtn.removeEventListener("click", displayNote)
+  }
+}
 
   
 
